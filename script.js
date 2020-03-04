2b39ffc739a2c30f588cdbe28c5d59950a6ff569
luqqy <luckyoula@gmail.com>
2020-03-04 10:07:32 +0300

    updated js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
   $("#checkout").click(function() {
     var type = $("#type").val();
-    alert(type + "");
+    var flavor=$ ("#flavor").val();
+    var size= $("#size").val();
+    var crust= $("#crust").val();
+    var toppings= $("#toppings").val();
+    
+
+    
+    alert( "" + type + " a size of " + size + ", a flavor " + flavor +crust+""+toppings+"");
+    alert("thank you for making your order")
+    prompt("enter you location")
+    alert("thank you for giving your location")
+    alert("You delivery will be made soon,continue shopping with us")
   });
 });
 
